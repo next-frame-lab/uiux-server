cbf29373471bda3fffd266456a06c44fb5ebcdde
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-07-28 23:04:28 +0900

    #11 feat(domain.performance): 공연 목록 페이지 UI 수정, Header & Footer.tsx 분리 - 공연 목록 페이지 <a> 태그를 <button> 태그로 수정했습니다. - Header와 Footer를 재사용하기 위해 components/layout 디렉토리에 Footer & Header.tsx 분리하였습니다.
    Resolves: #11
    See also: None

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,38 +1,10 @@
+import Header from "../../components/layout/Header.tsx";
+
 export default function LoginPage() {
 	return (
 		<div className="min-h-screen bg-gray-50 flex flex-col">
 			{/*상단 부분*/}
-			<header className="flex items-center justify-between px-15 py-6 bg-[#FBFBFB] border-b border-[#E8EDF5]">
-				<div className="flex items-center gap-4">
-					<img
-						src="/src/assets/images/logo.png"
-						className="w-10 h-10"
-						alt="NextFrame 로고 이미지입니다"
-					/>
-					<p className="text-xl font-bold">NextFrame</p>
-				</div>
-				<nav className="flex items-center gap-3 space-x-3">
-					<a className="hover:bg-gray-200">Main</a>
-					<a className="hover:bg-gray-200">Shows</a>
-					<a className="hover:bg-gray-200">About Us</a>
-					<a className="hover:bg-gray-200">My Page</a>
-					<a className="bg-gray-100 px-5 py-2 rounded-full font-semibold hover:bg-gray-200">
-						Login
-					</a>
-					<a className="bg-gray-100 px-3 py-2 rounded-full font-semibold hover:bg-gray-200">
-						<img
-							src="/public/icons/change_mode.png"
-							alt="라이트모드/다크모드 변경용 버튼입니다"
-						/>
-					</a>
-					<a className="bg-gray-100 px-3 py-2 rounded-full font-semibold hover:bg-gray-200">
-						<img
-							src="/public/icons/change_language.png"
-							alt="언어 변경용 버튼입니다"
-						/>
-					</a>
-				</nav>
-			</header>
+			<Header />
 
 			{/*하단 로그인 부분*/}
 			<main className="flex flex-1 items-center justify-center bg-[#FBFBFB]">
