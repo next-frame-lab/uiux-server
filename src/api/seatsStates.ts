3b92a715994c794dc3ba07f62a847a1cf6021a9a
Geunwon Park <rmsdnjsaos@gmail.com>
2025-08-20 14:17:02 +0900

    #29 test(domain.seat) 선택 공연 일정 & 공연장 좌석 API MOCKING
    - MSW(API MOCKING)을 통해 공연장 좌석 정보 조회
     - 정상적으로 공연 좌석 조회
     - 좌석 선택 제한 적용
     - 좌석 등급에 따른 가격이 실시간으로 반영
    
    Resolves: #29
    See also: #26

---
+++ src/api/seatsStates.ts
@@ -0,0 +1,13 @@
+const fetchSeatsStates = async (id: string) => {
+	const res = await fetch(`/stadiums/${id}/seat-states`, {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+
+	if (!res.ok) throw new Error("공연 목록을 불러오지 못했습니다.");
+	return res.json();
+};
+
+export default fetchSeatsStates;
