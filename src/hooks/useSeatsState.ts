5382f9c9a5acf01d4dca5ecb5c9db59c8a9ce54b
Geunwon Park <rmsdnjsaos@gmail.com>
2025-08-25 19:12:59 +0900

    #30 feat(domain.seat) 좌석 도메인 코드 리뷰 반영
    - 코드 리뷰 반영하였습니다.
    - n초마다 api 호출 & 새로고침 기능은 필요없다고 판단되어 제거하였습니다.
    
    Resolves: #30
    See also: None

--- src/hooks/useSeatsState.ts
+++ src/hooks/useSeatsState.ts
@@ -2,18 +2,11 @@ import { useQuery } from "@tanstack/react-query";
 import { seatStateData } from "../types/ApiDataTypes.ts";
 import fetchSeatsStates from "../api/seatsStates.ts";
 
-export default function useSeatsState(
-	scheduleId: string,
-	enabled: boolean,
-	hasSelection: boolean
-) {
+export default function useSeatsState(scheduleId: string, enabled: boolean) {
 	return useQuery<seatStateData[]>({
 		queryKey: ["seatsState", scheduleId],
 		queryFn: () => fetchSeatsStates(scheduleId),
 		enabled,
-		staleTime: 3000,
-		refetchInterval: hasSelection ? 3000 : 5000,
-		refetchIntervalInBackground: true,
 		refetchOnWindowFocus: true,
 	});
 }
