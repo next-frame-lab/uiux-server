8e30e395752b25ba7a0a497a6abaa34addb0e4c8
Geunwon Park <rmsdnjsaos@gmail.com>
2025-09-19 18:42:36 +0900

    fix(release): Jest - env 설정 오류 해결

--- src/api/ReviewLikes.ts
+++ src/api/ReviewLikes.ts
@@ -3,8 +3,6 @@ import getEnvVar from "../utils/env.ts";
 
 const VITE_BACKEND_SRT_API = getEnvVar("VITE_BACKEND_SRT_API");
 
-const { VITE_BACKEND_SRT_API } = import.meta.env;
-
 const fetchReviewLikes = async (id: string, like: boolean) => {
 	return authedJSON(`${VITE_BACKEND_SRT_API}/api/v1/reviews/${id}/likes`, {
 		method: "POST",
