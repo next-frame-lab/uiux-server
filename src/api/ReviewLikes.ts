72a3652171f95b18e2f058791881e06af9f0401b
GeunwonPark <101256141+Geunone2@users.noreply.github.com>
2025-10-03 00:56:54 +0900

    Revert "fix(release): Jest - env 설정 오류 해결"
    This reverts commit 8e30e395752b25ba7a0a497a6abaa34addb0e4c8.

--- src/api/ReviewLikes.ts
+++ src/api/ReviewLikes.ts
@@ -3,6 +3,8 @@ import getEnvVar from "../utils/env.ts";
 
 const VITE_BACKEND_SRT_API = getEnvVar("VITE_BACKEND_SRT_API");
 
+const { VITE_BACKEND_SRT_API } = import.meta.env;
+
 const fetchReviewLikes = async (id: string, like: boolean) => {
 	return authedJSON(`${VITE_BACKEND_SRT_API}/api/v1/reviews/${id}/likes`, {
 		method: "POST",
