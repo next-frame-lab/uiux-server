eb71de6ec3daff12b405041de1af2bdb58ef8bf0
GeunwonPark <101256141+Geunone2@users.noreply.github.com>
2025-10-03 00:56:54 +0900

    Revert "chore(release): 2025-09배포(v1)"
    This reverts commit e6a0293f8315d142a35b40a038e3afa8650941b6.

--- src/api/seats.ts
+++ src/api/seats.ts
@@ -3,8 +3,6 @@ import { selectSeatsData } from "../types/ApiDataTypes.ts";
 import getEnvVar from "../utils/env.ts";
 const VITE_BACKEND_SRT_API = getEnvVar("VITE_BACKEND_SRT_API");
 
-const { VITE_BACKEND_SRT_API } = import.meta.env;
-
 const fetchSeats = async (id: string): Promise<selectSeatsData> => {
 	return authedJSON(
 		`${VITE_BACKEND_SRT_API}/api/v1/stadiums/${id}/seat-definitions`,
