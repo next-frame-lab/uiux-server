127a3a4e630b6a74e7ec76c27d06b30c562e459d
Geunwon Park <rmsdnjsaos@gmail.com>
2025-10-23 17:26:55 +0900

    #90 bug(api): .env 키 값 변경 & 그에 따른 API 분기 처리
    - 새로운 키 값으로 변경
    - 키 값 변경을 통한, API 분기 처리 진행
    - CI/CD 검증 시, 필요없는 키 제거
    
    Resolves: #90
    See also: None

--- src/api/popularPerformance.ts
+++ src/api/popularPerformance.ts
@@ -1,10 +1,7 @@
 import { PopularPerformanceData } from "../types/ApiDataTypes.ts";
 import { publicJSON } from "../lib/apiClient.ts";
 
-const apiUrl =
-	process.env.MODE === "development"
-		? process.env.BACKEND_DEVELOPMENT_SRT_API
-		: process.env.BACKEND_SRT_API;
+const apiUrl = process.env.BACKEND_SRT_API;
 
 const fetchPopularPerformances = async (): Promise<PopularPerformanceData> => {
 	const url = `${apiUrl}/api/v1/performances/top10`;
