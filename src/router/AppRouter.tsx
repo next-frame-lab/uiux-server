bc7579ccd02eeaa71a4c76c4d46771844c3d780b
Eunhyeon Kang <kaeuhy_0315@naver.com>
2025-12-10 14:27:38 +0900

    #113 feat: 팀 소개 페이지 구현 & 로그인 페이지 디자인 리뉴얼 - loginPage.tsx의 디자인을 리뉴얼하였습니다.   - 필요없는 이미지 아이콘 삭제   - 규격에 맞는 OAuth 로그인 아이콘으로 변경 - src/pages/about/AboutPage.tsx 팀 소개 페이지 구현하였습니다. - 팀 소개 페이지 AppRouter 적용하였습니다.
    Resolves: #105, #113
    See also: None

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -9,6 +9,7 @@ import LoginPage from "../pages/login/LoginPage.tsx";
 import PerformancePage from "../pages/performance/PerformancePage.tsx";
 import PerformanceDetailPage from "../pages/performance/PerformanceDetailPage.tsx";
 import MyPage from "../pages/mypage/MyPage.tsx";
+import AboutPage from "../pages/about/AboutPage.tsx";
 import KakaoRedirectPage from "../pages/auth/KakaoRedirectPage.tsx";
 import SeatSelectPage from "../pages/reservation/SeatSelectPage.tsx";
 import PaymentPage from "../pages/payment/PaymentPage.tsx";
@@ -20,19 +21,13 @@ import ErrorBoundary from "../components/ui/ErrorBoundary.tsx";
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route errorElement={<ErrorBoundary />}>
-			{/* / */}
 			<Route index element={<MainPage />} />
-
-			{/* /login /mypage */}
 			<Route path="login" element={<LoginPage />} />
 			<Route path="mypage" element={<MyPage />} />
-
-			{/* /auth/... */}
+			<Route path="about" element={<AboutPage />} />
 			<Route path="auth">
 				<Route path="kakao/callback" element={<KakaoRedirectPage />} />
 			</Route>
-
-			{/* /performances/... */}
 			<Route path="performances">
 				<Route index element={<PerformancePage />} />
 				<Route path=":id">
@@ -40,19 +35,16 @@ const router = createBrowserRouter(
 					<Route path="seats" element={<SeatSelectPage />} />
 				</Route>
 			</Route>
-
-			{/* /payments */}
 			<Route path="payments">
 				<Route index element={<PaymentPage />} />
 				<Route path="success" element={<SuccessPage />} />
 				<Route path="fail" element={<FailPage />} />
 			</Route>
-
-			{/* 404 */}
 			<Route path="*" element={<NotFoundPage />} />
 		</Route>
 	)
 );
+
 export default function AppRouter() {
 	return <RouterProvider router={router} />;
 }
